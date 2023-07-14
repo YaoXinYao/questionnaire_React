d1df3033eca69e054740dab088976c8ff2f843eb
Administrator <3190493163@qq.com>
2023-07-14 10:38:01 +0800

    build:构建

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>不好</p>
+    </div>
+  );
+}
+
+export default App;
